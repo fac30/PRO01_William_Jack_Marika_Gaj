d0f4074dcacacbc5396205e0e31cf38fc9087e57
Gaajia <m.gajia1998@gmail.com>
2024-09-10 17:15:04 +0100

    working on model

--- js/movie-cards/movie-cards.js
+++ js/movie-cards/movie-cards.js
@@ -1,27 +1,37 @@
-const movieCards = document.getElementById('movie-cards');
 
-console.log(movieCards)
-
-
+const movieCards = document.getElementById('movie-cards')
 import cards  from "./moviedata.js"
 console.log(cards)
 
-  function appendCards() {   
-         movieCards.innerHTML = cards.map(card => `
-         <div id="${card.id}"class="movie-card" >
-         <img id="poster" src="${card.poster}">
-         </div>`
-         ).join('\n')
-
-          }
-
+function appendCards() {   
+  movieCards.innerHTML = cards.map(card => `
+  <div id="${card.id}"class="movie-card">
+  <img id="poster" src="${card.poster}">
+  </div>`
+  ).join('\n')
+  
+}
 export default appendCards
-// movieCards.addEventListener('click', (cards)=>{
-    
-// })
-
-        
 
 
         
-        
+movieCards.addEventListener('click', (e) => {
+
+  //forEach(e.id === card.id) { showModal()}
+
+  //call moddal
+
+})
+
+function renderModal(){
+  movieCards.innerHTML = cards.map(card => `
+  <div id="${card.id}"class="movie-card">
+  <h2>${card.title}</h2>
+  <h3>${card.director}</h3>
+  <p>${card.year}</p>
+  <p>${card.synopsis}</p>
+  <button>close</button>
+  </div>`
+  ).join('\n')
+}
+
