15698108d1f7ec61ed78a405cc2852ebe6b271ed
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-09-12 10:47:53 +0100

    fix: tabindex for modal and some responsive styling

--- js/movie-cards/movie-cards.js
+++ js/movie-cards/movie-cards.js
@@ -1,13 +1,12 @@
-
-const movieCards = document.getElementById('movie-cards')
+const movieCards = document.getElementById("movie-cards");
 
 import cards from "./moviedata.js";
 
- 
-
-function appendCards() {   
-  movieCards.innerHTML = cards.map(card => `
-  <div id="${card.id}" class="movie-card">
+function appendCards() {
+  movieCards.innerHTML = cards
+    .map(
+      (card) => `
+  <div tabindex="0" id="${card.id}" class="movie-card">
   <img id="poster" src="${card.poster}">
   
   
@@ -16,61 +15,60 @@ function appendCards() {
       <h3>${card.director}</h3>
       <p>${card.year}</p>
       <p>${card.synopsis}</p>
-      <button id="close-modal" class="close-modal">CLOSE</button></div>
+      <button tabindex="0" id="close-modal" class="close-modal">CLOSE</button></div>
   </div>
   
   
   `
-  ).join('\n')
-  
+    )
+    .join("\n");
 }
 
- const movieEventListener = () => {
-  let selectedModal = ""
+const movieEventListener = () => {
+  let selectedModal = "";
 
-  const movieCardElements = document.querySelectorAll(".movie-card")
-  const movieModals = document.querySelectorAll(".movie-modal")
-  const modalClose = document.querySelectorAll(".close-modal")
+  const movieCardElements = document.querySelectorAll(".movie-card");
+  const movieModals = document.querySelectorAll(".movie-modal");
+  const modalClose = document.querySelectorAll(".close-modal");
 
   movieCardElements.forEach((element) => {
+    element.addEventListener("click", function () {
+      console.log("triggered");
+      selectedModal = `movie-modal-${element.id}`;
+      renderModal();
+    });
+  });
 
-    element.addEventListener("click", function() {
-      console.log("triggered")
-      selectedModal = `movie-modal-${element.id}`
-      renderModal()
-    })
+  movieCardElements.forEach((element) => {
+    element.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        console.log("triggered");
+        selectedModal = `movie-modal-${element.id}`;
+        renderModal();
+      }
+    });
+  });
 
-  })
-  
-  modalClose.forEach((buttonEl)=> {
-    buttonEl.addEventListener('click', function(event) {
+  modalClose.forEach((buttonEl) => {
+    buttonEl.addEventListener("click", function (event) {
       event.stopPropagation();
       selectedModal = "";
-      console.log(selectedModal)
+      console.log(selectedModal);
       renderModal();
-    })
-  })
-
-
-
-
+    });
+  });
 
   const renderModal = () => {
-
     movieModals.forEach((modal) => {
-
       if (modal.id === selectedModal) {
-        console.log(selectedModal)
-        modal.classList.add("show")
-
-      } if (modal.id !== selectedModal) {
-        modal.classList.remove("show")
+        console.log(selectedModal);
+        modal.classList.add("show");
       }
-    })
-  }
-}
+      if (modal.id !== selectedModal) {
+        modal.classList.remove("show");
+      }
+    });
+  };
+};
 
-
-
-
-export  {appendCards, movieEventListener}
+export { appendCards, movieEventListener };
